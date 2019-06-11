619ab504b6c38e92aa096f621d0ec0102a294f3a
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-06-11 14:29:21 -0400

    working server intial state

---
+++ src/bookmarks.js
@@ -0,0 +1,8 @@
+const bookmarks = [{
+    id: "cjozyzcil0000lxygs3gyg2mr",
+    title: "Google",
+    url: "https://wwww.google.com",
+    description: "Where we find everthing else",
+    rating: 5
+}];
+module.exports = bookmarks
